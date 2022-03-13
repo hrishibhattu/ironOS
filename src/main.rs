89b58762d7b309b870788b63bd5970b90961ca48
Hrishikesh Bhattu <hrishibhattu123@gmail.com>
2022-03-13 15:41:54 +0530

    Create a freestanding (not dependant on underlying OS) binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+#[panic_handler] // this function will be called if the program panics
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this is the entry point since the linker looks for a function
+    // named `_start` by default
+    loop {}
 }
